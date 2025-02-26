fddbea4c9497ee552ab9af844a2d3c741f2d36fc
lupesta <lukepestaille@hotmail.co.uk>
2025-02-26 11:38:07 +0000

    simplified the image loading/rendering

--- main.py
+++ main.py
@@ -12,32 +12,6 @@ class Actions(Enum):
     RECHARGE = 5
     TRANSMIT = 6
 
-# 0 (0,0)
-# 1 (1,0)
-# 2 (2,0)
-# 3 (3,0)
-# 4 (4,0)
-# 5 (0,1)
-# 6 (1,1)
-# 7 (2,1)
-# 8 (3,1)
-# 9 (4,1)
-# 10 (0,2)
-# 11 (1,2)
-# 12 (2,2)
-# 13 (3,2)
-# 14 (4,2)
-# 15 (0,3)
-# 16 (1,3)
-# 17 (2,3)
-# 18 (3,3)
-# 19 (4,3)
-# 20 (0,4)
-# 21 (1,4)
-# 22 (2,4)
-# 23 (3,4)
-# 24 (4,4)
-
 class Robot():
     def __init__(self):
         self.position = (0, 0)
@@ -53,6 +27,7 @@ class Mars_Environment():
         self.clock = None
         self.q_table = [[0 for _ in range(len(Actions))] for _ in range(size * size)]
         self.rewards = [[0 for _ in range(size * size)] for _ in range(size * size)]
+        self.robot = [(0,0)]
         self.rocks = [(1,2), (3,3), (2,4)]
         self.transmiter_stations = [(4,4)]
         self.cliffs = [(2,3), (1,1)]
@@ -109,106 +84,66 @@ class Mars_Environment():
                 self.rewards[downhill[0] + self.size*(downhill[1]+1)][downhill[0] + self.size*downhill[1]] = 5
 
 
+    def process_image(self, filename, scale_factor):
+        path = os.path.join(os.getcwd(), "images", filename)
+        image = pygame.image.load(path)
+        image = pygame.transform.scale(image, (int(self.grid_size * scale_factor), int(self.grid_size * scale_factor)))
+        image = image.convert_alpha()
+        return image
+
     def load_images(self):
-        _robot = pygame.image.load(f"{os.getcwd()}/images/robot.png")
-        _robot = pygame.transform.scale(_robot, (int(self.grid_size) * 0.8, int(self.grid_size) * 0.8))
-        _robot.convert()
-
-        _rock = pygame.image.load(f"{os.getcwd()}/images/rock.png")
-        _rock = pygame.transform.scale(_rock, (int(self.grid_size) * 0.6, int(self.grid_size) * 0.6))
-        _rock.convert()
-
-        _transmiter = pygame.image.load(f"{os.getcwd()}/images/antenna.png")
-        _transmiter = pygame.transform.scale(_transmiter, (int(self.grid_size) * 0.6, int(self.grid_size) * 0.6))
-        _transmiter.convert()
-
-        _cliff = pygame.image.load(f"{os.getcwd()}/images/cliff.png")
-        _cliff = pygame.transform.scale(_cliff, (int(self.grid_size) * 0.6, int(self.grid_size) * 0.6))
-        _cliff.convert()
-
-        _uphill = pygame.image.load(f"{os.getcwd()}/images/uphill.png")
-        _uphill = pygame.transform.scale(_uphill, (int(self.grid_size) * 0.6, int(self.grid_size) * 0.6))
-        _uphill.convert()
-
-        _downhill = pygame.image.load(f"{os.getcwd()}/images/downhill.png")
-        _downhill = pygame.transform.scale(_downhill, (int(self.grid_size) * 0.6, int(self.grid_size) * 0.6))
-        _downhill.convert()
-
-        _battery = pygame.image.load(f"{os.getcwd()}/images/battery.png")
-        _battery = pygame.transform.scale(_battery, (int(self.grid_size) * 0.6, int(self.grid_size) * 0.6))
-        _battery.convert()
-
+        _robot = self.process_image("robot.png", 0.6)
+        _rock = self.process_image("rock.png", 0.6)
+        _transmiter = self.process_image("antenna.png", 0.6)
+        _cliff = self.process_image("cliff.png", 0.6)
+        _uphill = self.process_image("uphill.png", 0.6)
+        _downhill = self.process_image("downhill.png", 0.6)
+        _battery = self.process_image("battery.png", 0.6)
         return _robot, _rock, _transmiter, _cliff, _uphill, _downhill, _battery
-    
-    def render_images(self, _rock, _transmiter, _cliff, _uphill, _downhill, _battery):
-            for rock in self.rocks:
-                rock_pos = (rock[0] * self.grid_size + self.grid_size * 0.2,
-                            rock[1] * self.grid_size + self.grid_size * 0.2)
-                self.display.blit(_rock, rock_pos)
 
-            for transmiter in self.transmiter_stations:
-                transmiter_pos = (transmiter[0] * self.grid_size + self.grid_size * 0.2,
-                                  transmiter[1] * self.grid_size + self.grid_size * 0.2)
-                self.display.blit(_transmiter, transmiter_pos)
+    def blit_objects(self, image, objects, offset=0.2):
+        for obj in objects:
+            pos = (obj[0] * self.grid_size + self.grid_size * offset, obj[1] * self.grid_size + self.grid_size * offset)
+            self.display.blit(image, pos)
 
-            for cliff in self.cliffs:
-                cliff_pos = (cliff[0] * self.grid_size + self.grid_size * 0.2,
-                             cliff[1] * self.grid_size + self.grid_size * 0.2)
-                self.display.blit(_cliff, cliff_pos)
-            
-            for uphill in self.uphills:
-                uphill_pos = (uphill[0] * self.grid_size + self.grid_size * 0.2,
-                              uphill[1] * self.grid_size + self.grid_size * 0.2)
-                self.display.blit(_uphill, uphill_pos)
-
-            for downhill in self.downhills:
-                downhill_pos = (downhill[0] * self.grid_size + self.grid_size * 0.2,
-                                downhill[1] * self.grid_size + self.grid_size * 0.2)
-                self.display.blit(_downhill, downhill_pos)
-
-            for battery in self.batery_stations:
-                battery_pos = (battery[0] * self.grid_size + self.grid_size * 0.2,
-                               battery[1] * self.grid_size + self.grid_size * 0.2)
-                self.display.blit(_battery, battery_pos)
-                
+    def render_images(self, _robot, _rock, _transmiter, _cliff, _uphill, _downhill, _battery):
+        self.blit_objects(_robot, self.robot)
+        self.blit_objects(_rock, self.rocks)
+        self.blit_objects(_transmiter, self.transmiter_stations)
+        self.blit_objects(_cliff, self.cliffs)
+        self.blit_objects(_uphill, self.uphills)
+        self.blit_objects(_downhill, self.downhills)
+        self.blit_objects(_battery, self.batery_stations)
 
     def render(self):
         pygame.init()
         pygame.display.init()
-        self.display = pygame.display.set_mode((self.size, self.size))
+        self.display = pygame.display.set_mode((self.window, self.window))
         self.clock = pygame.time.Clock()
-        screen = pygame.display.set_mode((self.window, self.window))
         pygame.display.set_caption('Mars Space Exploration')
         self.grid_size = int(self.window/self.size)
         self.grid_colour = (200, 200, 200)
-        self.line_colour = (100, 100, 100)
-
+        self.line_colour = (0, 0, 0)
         _robot, _rock, _transmiter, _cliff, _uphill, _downhill, _battery = self.load_images()
 
-        screen_on = True
-        while screen_on:
+        display_on = True
+        while display_on:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
-            screen.fill(self.grid_colour)
+            self.display.fill(self.grid_colour)
             for x in range(0, self.window, self.grid_size):
-                pygame.draw.line(screen, self.line_colour, (x, 0), (x, self.window))
+                pygame.draw.line(self.display, self.line_colour, (x, 0), (x, self.window))
             for y in range(0, self.window, self.grid_size):
-                pygame.draw.line(screen, self.line_colour, (0, y), (self.window, y))
+                pygame.draw.line(self.display, self.line_colour, (0, y), (self.window, y))
 
-            robot_pos = (self.robot.position[0] * self.grid_size + self.grid_size * 0.1,
-                         self.robot.position[1] * self.grid_size + self.grid_size * 0.1)
-
-            self.display.blit(_robot, robot_pos)
-
-            self.render_images(_rock, _transmiter, _cliff, _uphill, _downhill, _battery)
+            self.render_images(_robot, _rock, _transmiter, _cliff, _uphill, _downhill, _battery)
 
             pygame.display.flip()
 
     def update_q_table(self):
         pass
-
 
 mars_environment = Mars_Environment(5)
