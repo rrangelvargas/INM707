fbdcf302fab1b6a511a01724b6b073a98ab6d14d
lupesta <lukepestaille@hotmail.co.uk>
2025-05-10 11:01:25 +0100

    added comments

--- main.py
+++ main.py
@@ -4,57 +4,54 @@ from deep_q_environment import MarsEnv, DeepQAgent
 import torch
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # the device, either cuda or cpu
 
-with open("config.yaml", "r") as f:
+with open("config.yaml", "r") as f: # loads the settings from the yaml file
     configs = yaml.safe_load(f)
 
-def train_model(config, run_name, display=False):
+def train_model(config, run_name, display=False): # trains the deep q learning agent
     print('----------------------------------------------------')
     print(f'Running {run_name}...')
     print("Double DQN: ", config["params"]["double_dqn"])
     print("Prioritised: ", config["params"]["prioritised"])
     print('----------------------------------------------------')
 
-    env = MarsEnv(config)
-    agent = DeepQAgent(env, display=display, params=config["params"])
-    agent.train(config["params"], run_name, save_graphs=True)
-    agent.save_results(run_name, config["params"]["num_episodes"], config["params"]["save_checkpoint"])
+    env = MarsEnv(config) # passes the config settings to load the mars environment
+    agent = DeepQAgent(env, display=display, params=config["params"]) # loads the agent based on the parameters passed
+    agent.train(config["params"], run_name, save_graphs=True) # trains the agent using the train method
+    agent.save_results(run_name, config["params"]["num_episodes"], config["params"]["save_checkpoint"]) # saves the results of the experiment
     return agent
 
-def test_model(config, run_name, display=False):
+def test_model(config, run_name, display=False): # tests the model using the weights trained weights
     print('----------------------------------------------------')
     print(f'Testing {run_name}...')
     print("Double DQN: ", config["params"]["double_dqn"])
     print("Prioritised: ", config["params"]["prioritised"])
     print('----------------------------------------------------')
 
-    num_episodes = config["params"]["num_episodes"]
+    num_episodes = config["params"]["num_episodes"] # the number of episodes to run for
 
-    env = MarsEnv(config)
-    agent = DeepQAgent(env, display=display, params=config["params"])
+    env = MarsEnv(config) # passes the config settings to load the mars environment
+    agent = DeepQAgent(env, display=display, params=config["params"]) # loads the agent based on the parameters passed
     
-    # Load checkpoint
-    checkpoint_path = f"checkpoints/{run_name}_2000.pth"
-    agent.policy_net.load_state_dict(torch.load(checkpoint_path, map_location=device))
+    checkpoint_path = f"checkpoints/{run_name}_2000.pth" # defines the checkpoint path
+    agent.policy_net.load_state_dict(torch.load(checkpoint_path, map_location=device)) # loads the checkpoint weights into the policy network
     
-    # Run test episodes
-    agent.train(config["params"], run_name, save_graphs=False)
-    agent.save_results(run_name, num_episodes, False)
+    agent.train(config["params"], run_name, save_graphs=False) # trains the agent using the train method
+    agent.save_results(run_name, num_episodes, False)  # saves the results of the experiment
 
 if __name__ == "__main__":
     env = input("Which environment do you want to run? (1 - simple / 2 - deep_q [default])\n")
     if not env:
         env = "2"
 
-    display_input = input("Do you want to display the environment? (y / n [default])\n")
+    display_input = input("Do you want to display the environment? (y / n [default])\n") # asks the user if they want the game window to be displayed
     if not display_input:
         display_input = "n"
 
     display = True if display_input == "y" else False
 
-    if env == "1":
-        ## Simple Q-Learning
+    if env == "1": # the simple q-learning environment
         mars_environment = Mars_Environment(
             config = configs["simple_game"],
             max_epsilon = 1,
@@ -72,20 +69,18 @@ if __name__ == "__main__":
             display=display
         )
 
-        mars_environment.run()
+        mars_environment.run() # runs the simple environment
 
-    elif env == "2":
+    elif env == "2": # the deep-q environment
         test = input("Do you want to test the best model (prioritised_double_dqn)? (y / n [default])\n")
         if not test:
             test = "n"
 
         test = True if test == "y" else False
 
-        ## Deep Q-Learning
         config = configs["deep_q_game"]
 
-        if test:
-            # Testing phase
+        if test: # the testing settings
             test_config = config.copy()
             test_config["params"]["num_episodes"] = 500
             test_config["params"]["epsilon_start"] = 0.05
@@ -95,28 +90,28 @@ if __name__ == "__main__":
 
             test_model(test_config, "prioritised_double_dqn", display)
         else:
-            training_model = input("Which model do you want to train? (1 - baseline / 2 - double_dqn / 3 - prioritised_double_dqn [default])\n")
-            if not training_model:
+            training_model = input("Which model do you want to train? (1 - baseline / 2 - double_dqn / 3 - prioritised_double_dqn [default])\n") # asks the user to train a specific model
+            if not training_model: # defaults to double deep q and PER
                 print("Defaulting to prioritised_double_dqn")
                 training_model = "3"
 
-            if training_model not in ["1", "2", "3"]:
+            if training_model not in ["1", "2", "3"]: # ifd not in the valid model numbers
                 print("Invalid input. Please enter 1, 2, or 3.")
                 exit()
 
             training_model = int(training_model)
 
-            if training_model == 1:
+            if training_model == 1: # using the baseline model
                 config["params"]["double_dqn"] = False
                 config["params"]["prioritised"] = False
                 train_model(config, "baseline", display)
 
-            elif training_model == 2:
+            elif training_model == 2: # using the double deep-q model
                 config["params"]["double_dqn"] = True
                 config["params"]["prioritised"] = False
                 train_model(config, "double_dqn", display)
             
-            elif training_model == 3:
+            elif training_model == 3: # using double-deep q and PER
                 config["params"]["double_dqn"] = True
                 config["params"]["prioritised"] = True
                 train_model(config, "prioritised_double_dqn", display)
